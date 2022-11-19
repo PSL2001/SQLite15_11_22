c3d5ee8fa99950aac3caf27c33a5d613e363ceb0
PSL2001 <65461723+PSL2001@users.noreply.github.com>
2022-11-19 23:05:35 +0100

    Creado AddUpdateActivity

---
+++ app/src/main/java/com/example/sqlite/adapter/ArticulosAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.sqlite.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sqlite.models.Articulo
+
+class ArticulosAdapter(private val lista:MutableList<Articulo>): RecyclerView.Adapter<ArticulosViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticulosViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: ArticulosViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/sqlite/adapter/ArticulosViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.sqlite.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ArticulosViewHolder(v: View): RecyclerView.ViewHolder(v) {
+
+}
