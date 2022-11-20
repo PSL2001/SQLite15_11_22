fa1551f5500ed3aee0ec459fbed88c0b1e73e7c6
PSL2001 <65461723+PSL2001@users.noreply.github.com>
2022-11-20 14:35:31 +0100

    Creada conexion y seteado layouts

--- app/src/main/java/com/example/sqlite/MainActivity.kt
+++ app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -1,14 +1,48 @@
 package com.example.sqlite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sqlite.adapter.ArticulosAdapter
 import com.example.sqlite.databinding.ActivityMainBinding
+import com.example.sqlite.db.BaseDatos
+import com.example.sqlite.models.Articulo
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var conexion: BaseDatos
+    lateinit var adapter: ArticulosAdapter
+    var lista = mutableListOf<Articulo>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //Creamos la conexion
+        conexion = BaseDatos(this)
+        //Seteamos el recycler
+        setRecycler()
+        //Seteamos los listeners
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.fbtnAdd.setOnClickListener {
+            startActivity(Intent(this, AddUpdateActivity::class.java))
+        }
+    }
+
+    private fun setRecycler() {
+        lista = conexion.readAll()
+        if (lista.size == 0) {
+            binding.tvVacio.visibility = View.VISIBLE
+            return
+        }
+        val layoutManager = LinearLayoutManager(this)
+        binding.rcAticulos.layoutManager = layoutManager
+        adapter = ArticulosAdapter(lista)
+        binding.rcAticulos.adapter = adapter
     }
 }
