47b135bcef8bed07e7990cec9e8881d880757263
PSL2001 <65461723+PSL2001@users.noreply.github.com>
2022-11-20 14:09:57 +0100

    Creado Adapter

--- app/src/main/java/com/example/sqlite/adapter/ArticulosAdapter.kt
+++ app/src/main/java/com/example/sqlite/adapter/ArticulosAdapter.kt
@@ -1,20 +1,23 @@
 package com.example.sqlite.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sqlite.R
 import com.example.sqlite.models.Articulo
 
 class ArticulosAdapter(private val lista:MutableList<Articulo>): RecyclerView.Adapter<ArticulosViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticulosViewHolder {
-
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.article_layout, parent, false)
+        return ArticulosViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ArticulosViewHolder, position: Int) {
-
+        holder.render(lista[position])
     }
 
     override fun getItemCount(): Int {
-
+        return lista.size
     }
 
 }
--- app/src/main/java/com/example/sqlite/adapter/ArticulosViewHolder.kt
+++ app/src/main/java/com/example/sqlite/adapter/ArticulosViewHolder.kt
@@ -2,7 +2,16 @@ package com.example.sqlite.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sqlite.databinding.ArticleLayoutBinding
+import com.example.sqlite.models.Articulo
 
 class ArticulosViewHolder(v: View): RecyclerView.ViewHolder(v) {
+    private val binding = ArticleLayoutBinding.bind(v)
+
+    fun render(articulo: Articulo) {
+        binding.tvNombre.text = articulo.nombre
+        binding.tvPrecio.text = articulo.precio.toString()
+        binding.tvStock.text = articulo.stock.toString()
+    }
 
 }
